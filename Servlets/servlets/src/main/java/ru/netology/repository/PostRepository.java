13994ff74717f926fe34ef75a63bb8e5da16e726
A.Medvedev <grizzly2400@mail.ru>
2025-03-22 16:12:32 +0300

    Servlets1

---
+++ Servlets/servlets/src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,40 @@
+package ru.netology.repository;
+
+import ru.netology.model.Post;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+// Stub
+
+public class PostRepository {
+  private final Map<Integer, Post> map = new HashMap<>();
+  private int count = 0;
+
+  public List<Post> all() {
+    return (List<Post>) map.values();
+  }
+
+  public Optional<Post> getById(long id) {
+    return Optional.ofNullable(map.get(id));
+  }
+
+  public Post save(Post post) {
+
+    if (post.getId() == 0) {
+      post.setId(count);
+      map.put(Math.toIntExact(post.getId()), post);
+      count++;
+
+    } else if (!(post.getId() == 0) & !map.containsKey(post.getId())) {
+      map.put(Math.toIntExact(post.getId()), post);
+    } else map.replace(Math.toIntExact(post.getId()), post);
+    return post;
+  }
+
+  public void removeById(long id) {
+    map.remove(id);
+  }
+}
