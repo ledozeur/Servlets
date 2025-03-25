028e70f12b561ac220db9f124294c9838dfe8313
ledozeur <grizzly2400@mail.ru>
2025-03-25 14:33:08 +0300

    Update MainServlet

--- Servlets/servlets/src/main/java/ru/netology/servlet/MainServlet.java
+++ Servlets/servlets/src/main/java/ru/netology/servlet/MainServlet.java
@@ -14,7 +14,7 @@ public class MainServlet extends HttpServlet {
 
   @Override
   public void init() {
-    final var context = new AnnotationConfigApplicationContext("ru.nikolai");
+    final var context = new AnnotationConfigApplicationContext("ru.netology");
     controller = context.getBean(PostController.class);
   }
 
